c860650960cf18216d06ed668e472d752e617ef2
KimoTerru <emirbajnazarov44@gmai.com>
2021-07-02 14:48:47 +0600

    Добавил UI

---
+++ app/src/main/java/it/kimoterru/manycoins/ui/detailed/DetailedFragment.kt
@@ -0,0 +1,7 @@
+package it.kimoterru.manycoins.ui.detailed
+
+import androidx.fragment.app.Fragment
+import it.kimoterru.manycoins.R
+
+class DetailedFragment : Fragment(R.layout.fragment_detailed) {
+}
